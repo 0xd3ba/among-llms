910934d402b6493d32270abb3a34db21ab5b0123
Debasish <0xd3ba@gmail.com>
2025-08-15 15:27:32 +0900

    add base class for parsing yaml files

--- allms/utils/parser.py
+++ allms/utils/parser.py
@@ -6,7 +6,29 @@ import yaml
 from allms.config import AppConfiguration
 
 
-class YAMLConfigFileParser:
+class BaseYAMLParser:
+    """ Base class for parsing YAML files """
+    def __init__(self, file_path: str | pathlib.Path):
+        self._file_path = pathlib.Path(file_path)
+        assert self._file_path.exists(), f"Provided file: {file_path} doesn't exist. Make sure the path is correct"
+
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def parse(self) -> dict:
+        with open(self._file_path, "r") as f:
+            yml_data = yaml.safe_load(f)
+
+        expected_keys = [getattr(self.__class__, attr) for attr in dir(self.__class__) if attr.startswith("key_")]
+        for ek in expected_keys:
+            assert ek in yml_data, f"Expecting {ek} to be present in the YAML file but is missing. Did you miss it?"
+        return yml_data
+    
+    def validate(self) -> None:
+        """ Validates the parsed arguments. Needs to raise RuntimeError if validation fails """
+        raise NotImplementedError
+
+
+class YAMLConfigFileParser(BaseYAMLParser):
     """ Parser for the user configuration file """
 
     key_ai_model: str = "model"
@@ -14,22 +36,13 @@ class YAMLConfigFileParser:
     key_max_agent_count: str = "maximumAgentCount"
 
     def __init__(self, file_path: str | pathlib.Path):
-        self._file_path = pathlib.Path(file_path)
-        assert self._file_path.exists(), f"Provided file: {file_path} doesn't exist. Make sure the path is correct"
-
-        self._logger = logging.getLogger(self.__class__.__name__)
+        super().__init__(file_path)
         self.ai_model: str | None = None
         self.reasoning_level: str | None = None
         self.max_agent_count: int | None = None
 
     def parse(self) -> None:
-        with open(self._file_path, "r") as f:
-            yml_data = yaml.safe_load(f)
-
-        expected_keys = [getattr(YAMLConfigFileParser, attr) for attr in dir(YAMLConfigFileParser) if attr.startswith("key_")]
-        for ek in expected_keys:
-            assert ek in yml_data, f"Expecting {ek} to be present in the config file but is missing. Did you edit it?"
-
+        yml_data = super().parse()
         self.ai_model = yml_data[self.key_ai_model].lower()
         self.reasoning_level = yml_data[self.key_reasoning_level].lower()
         self.max_agent_count = yml_data[self.key_max_agent_count]
