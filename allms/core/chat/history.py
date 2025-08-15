b5931b50b4b93eb23fec69252a40948352f1fa42
Debasish <0xd3ba@gmail.com>
2025-08-15 12:02:15 +0900

    add classes for chat message and chat history

---
+++ allms/core/chat/history.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+from dataclasses import dataclass, field
+
+from .message import ChatMessage
+
+
+@dataclass
+class ChatMessageHistory:
+    """ Class for a storing the history of chat messages """
+    # Maps message ID to the message for efficient retrieval and modification
+    _history: OrderedDict[str, ChatMessage] = field(default_factory=OrderedDict)
+
+    def add(self, message: ChatMessage) -> None:
+        """ Inserts the message into the history """
+        msg_id = message.id
+        assert not self.__has_message(msg_id), f"Can't insert as ID({msg_id}) of {message} already exists in the history"
+        self._history[msg_id] = message
+
+    def edit(self, msg_id: str, message: str, edited_by_you: bool = False) -> None:
+        """ Edits the contents of the message in the history """
+        assert self.__has_message(msg_id), f"Can't edit as ID({msg_id}) doesn't exist in the history"
+        self._history[msg_id].edit(message, edited_by_you)
+
+    def delete(self, msg_id: str, deleted_by_you: bool) -> None:
+        """ Deletes the contents of the message from the history without removing the message """
+        assert self.__has_message(msg_id), f"Can't delete as ID({msg_id}) doesn't exist in the history"
+        self._history[msg_id].delete(deleted_by_you)
+
+    def get(self, msg_id: str) -> ChatMessage:
+        """ Returns the message from the history """
+        assert self.__has_message(msg_id), f"Can't fetch as ID({msg_id}) doesn't exist in the history"
+        return self._history[msg_id]
+
+    def __has_message(self, msg_id: str) -> bool:
+        """ Helper method to check if a message exists in the history. Return True if exists """
+        return msg_id in self._history
---
+++ allms/core/chat/message.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from allms.config import AppConfiguration
+
+
+@dataclass
+class ChatMessageEditLog:
+    """ Class for edit-log of a chat message """
+    timestamp: str
+    prev_msg: str
+    edited_by_you: bool
+    deleted_by_you: bool
+
+
+@dataclass
+class ChatMessage:
+    """ Class for a single chat message """
+    id: str                            # The unique identifier of the message
+    timestamp: str                     # Timestamp in ISO format (YYYY-MM-DD HH:MM:SS) when the message was sent
+    sent_by: str                       # The originator of the message (can be you or a LLM)
+    sent_to: str                       # The recipient of the message (can be everyone or a single agent)
+    sent_by_you: bool                  # Set to true if it was YOU who sent the message
+    msg: str                           # The contents of the message
+
+    thought_process: str = ""          # The thought process behind the message (only set by the LLMs)
+    reply_to_id: Optional[str] = None  # If the message is a reply to a previous message
+    edited: bool = False               # Set to true if the message was edited (either by you or the LLM)
+    deleted: bool = False              # Set to true if the message was deleted (either by you or the LLM)
+    edited_by_you: bool = False        # Set to true if it was YOU who edited the message
+    deleted_by_you: bool = False       # Set to true if it was YOU who deleted the message
+
+    # Stores the history of the edits/delete of the message
+    history_log: list[ChatMessageEditLog] = field(default_factory=list)
+
+    def can_edit_or_delete(self) -> bool:
+        """ Returns True if allowed to edit/delete, else False """
+        return not self.deleted
+
+    def edit(self, message: str, edited_by_you: bool) -> None:
+        """ Edits the message with the new contents """
+        if not self.can_edit_or_delete():
+            return
+
+        prev_msg = self.msg
+        self.msg = message
+        self.edited = True
+        self.edited_by_you = edited_by_you
+
+        curr_ts = AppConfiguration.clock.current_timestamp_in_iso_format()
+        edit_log = ChatMessageEditLog(timestamp=curr_ts, prev_msg=prev_msg, edited_by_you=edited_by_you, deleted_by_you=False)
+        self.history_log.append(edit_log)
+
+    def delete(self, deleted_by_you: bool) -> None:
+        """ Deletes the message and updates the latest contents """
+        if not self.can_edit_or_delete():
+            return
+
+        prev_msg = self.msg
+        self.deleted = True
+        self.deleted_by_you = deleted_by_you
+        self.msg = "This message has been deleted"
+
+        curr_ts = AppConfiguration.clock.current_timestamp_in_iso_format()
+        edit_log = ChatMessageEditLog(timestamp=curr_ts, prev_msg=prev_msg, edited_by_you=False, deleted_by_you=deleted_by_you)
+        self.history_log.append(edit_log)
+        # Note: No longer edits or delete possible on this message from now on
