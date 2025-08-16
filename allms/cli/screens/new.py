ef72551bff111369cebb82dfd2f7825e13ebb428
Debasish <0xd3ba@gmail.com>
2025-08-16 12:30:53 +0900

    refactor class to inherit from base modal class

--- allms/cli/screens/new.py
+++ allms/cli/screens/new.py
@@ -1,22 +1,11 @@
-from textual.app import ComposeResult
-from textual.screen import ModalScreen
-from textual.widgets import Label, Footer
-
-from allms.config import BindingConfiguration, RunTimeConfiguration
+from allms.config import RunTimeConfiguration
 from allms.cli.widgets.new import NewChatroomWidget
 from allms.core.state import GameStateManager
+from .modal import BaseModalScreen
 
 
-class NewChatScreen(ModalScreen):
+class NewChatScreen(BaseModalScreen):
     """ Screen for creating new chatroom """
 
     def __init__(self, title: str, config: RunTimeConfiguration, state_manager: GameStateManager, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._title = title
-        self._config = config
-        self._state_manager = state_manager
-        self._new_chatroom_widget = NewChatroomWidget(self._title, self._config, self._state_manager)
-
-    def compose(self) -> ComposeResult:
-        yield self._new_chatroom_widget
-        yield Footer()
+        super().__init__(title, config, state_manager, NewChatroomWidget, *args, **kwargs)
